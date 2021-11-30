b7b4d153e6d41cd8addfe393f11d20311424cb47
Ponz-Tofu-N <shi.na.telebla3@gmail.com>
2021-11-30 10:06:53 +0900

    create tests for functionalCompare

--- 018/test/tst_variadic.cpp
+++ 018/test/tst_variadic.cpp
@@ -1,11 +1,26 @@
 #include <gtest/gtest.h>
+#include <functional>
 #include "../variadic.h"
 
 TEST(TstVariadic, minimum)
 {
-    EXPECT_EQ(1, minimum(1,2,3,4,5,6));
-    EXPECT_EQ(1, minimum(2,3,4,1,5,6));
-    EXPECT_EQ(1, minimum(2,3,4,5,6,1));
+    EXPECT_EQ(1, minimum(1, 2, 3, 4, 5, 6));
+    EXPECT_EQ(1, minimum(2, 3, 4, 1, 5, 6));
+    EXPECT_EQ(1, minimum(2, 3, 4, 5, 6, 1));
+}
+
+TEST(TstVariadic, functionalCompareLess)
+{
+    EXPECT_EQ(2, functionalCompare(std::less<>(), 2, 3, 4, 5, 6));
+    EXPECT_EQ(1, functionalCompare(std::less<>(), 2, 3, 4, 1, 5, 6));
+    EXPECT_EQ(1, functionalCompare(std::less<>(), 2, 3, 4, 5, 6, 1));
+}
+
+TEST(TstVariadic, functionalCompareGreater)
+{
+    EXPECT_EQ(6, functionalCompare(std::greater<>(), 2, 3, 4, 5, 6));
+    EXPECT_EQ(6, functionalCompare(std::greater<>(), 2, 3, 4, 1, 5, 6));
+    EXPECT_EQ(6, functionalCompare(std::greater<>(), 2, 3, 4, 5, 6, 1));
 }
 
 int main(int argc, char *argv[])
