fee0421f7fe3e18b2c921cc2f14be8abd5e383fe
Ponz-Tofu-N <shi.na.telebla3@gmail.com>
2022-04-17 23:30:07 +0900

    add memo

--- 035/directory.h
+++ 035/directory.h
@@ -5,11 +5,16 @@
 
 std::size_t directory_size(const std::string &dir)
 {
+    // 型名ではない名前空間の別名はusingではなくnamespaceで宣言する。
     namespace fs = std::filesystem;
 
     fs::path dir_path(dir);
+
+    //再帰なしのdirectory_iteratorというクラスもある。
     fs::recursive_directory_iterator rdir_itr(dir_path);
 
+    // 第3引数は初期値、第4引数は最終結果（returnは項ごとの結果ではなく、全体合算の値）
+    // その項は0でもreturnを0にすると、それまでの加算結果も0で上書きされる
     return std::accumulate(fs::begin(rdir_itr), fs::end(rdir_itr), 0UL,
                     [&](std::size_t total, const fs::directory_entry &e)
                     {
