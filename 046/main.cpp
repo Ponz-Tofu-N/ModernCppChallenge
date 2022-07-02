093999091df357c48bf147ec0bcbef0ab1d92da6
Ponz-Tofu-N <shi.na.telebla@gmail.com>
2022-07-02 18:17:37 +0900

    fix main.cpp

--- 046/main.cpp
+++ 046/main.cpp
@@ -15,41 +15,27 @@
 #include "alg.h"
 int main(int argc, char const* argv[])
 {
-  alg::ring_buffer<int> ring(5);
+  alg::ring_buffer<int> rb(5);
 
-  for (size_t i = 0; i < 5; i++)
-  {
-    ring.push(i);
-  }
-  std::cout << "first: ";
-  for (size_t i = 0; i < 5; i++)
-  {
-    std::cout << ring[i];
-  }
-  std::cout << std::endl;
+  rb.push(1);
+  rb.push(2);
+  rb.push(3);
 
-  for (size_t i = 5; i < 10; i++)
-  {
-    ring.push(i);
-  }
-  std::cout << "second: ";
-  for (size_t i = 0; i < 5; i++)
-  {
-    std::cout << ring[i];
-  }
-  std::cout << std::endl;
+  auto itr_b = rb.begin();
+  auto itr_e = rb.end();
 
-  auto itr_b = ring.begin();
-  std::cout << "itr(begin): " << *itr_b << std::endl;
+  std::cout << *itr_b << " " << 1 << std::endl;
+  std::cout << *(itr_e - 1) << " " << 3 << std::endl;
 
-  auto itr_e = ring.end();
-  std::cout << "it(end-2): " << *(itr_e + 2) << std::endl;
+  std::cout << *(itr_b + 7) << " " << 2 << std::endl;
 
-  for (size_t i = 1; i < 30; i++)
-  {
-    std::cout << *(itr_b - i);
-  }
-  std::cout << std::endl;
+  rb.push(4);
+  rb.push(5);
+  rb.push(6);
 
-  return 0;
+  itr_b = rb.begin();
+  itr_e = rb.end();
+
+  std::cout << *itr_b << " " << 2 << std::endl;
+  std::cout << *(itr_e - 1) << " " << 6 << std::endl;
 }
