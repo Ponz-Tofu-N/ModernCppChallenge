1e33a125c10ab2b2b9467a08d89633033386bb66
ponz-tofu-n <shi.na.telebla3@gmail.com>
2022-08-10 22:46:03 +0900

    refactoring code

--- 059/weasel.h
+++ 059/weasel.h
@@ -1,19 +1,20 @@
 #include <random>
 #include <string>
 #include <string_view>
+#include <sstream>
 
-const char alphabet[27] = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U', 'V','W','X','Y','Z',' ' };
+std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
 std::string ans = "METHINKS IT IS LIKE A WEASEL";
+std::random_device seed_gen;
+std::default_random_engine engine(seed_gen());
+
+std::uniform_int_distribution<> dist_aphabet(0, 26);
+std::uniform_int_distribution<> dist_percent(1, 100);
 
 bool is_match(std::string_view str) { return str == ans; }
+bool make_5percent() { return dist_percent(engine) <= 5; }
 
 void make_mutatable_string(std::string& original) {
-  std::random_device seed_gen;
-  std::default_random_engine engine(seed_gen());
-
-  std::uniform_int_distribution<> dist_aphabet(0, 26);
-  std::uniform_int_distribution<> dist_percent(1, 100);
-
   std::for_each(std::begin(original), std::end(original), [&](char& c) {
     if (make_5percent())
       c = alphabet[dist_aphabet(engine)];
@@ -21,23 +22,16 @@ void make_mutatable_string(std::string& original) {
 }
 
 std::string make_random_string() {
-  std::random_device seed_gen;
-  std::default_random_engine engine(seed_gen());
-
-  std::uniform_int_distribution<> dist(0, 26);
-
-  std::string random_string;
+  std::stringstream random_ss;
   for (size_t i = 0; i < ans.size(); i++)
   {
-    int index = dist(engine);
-    random_string += alphabet[index];
+    random_ss << alphabet[dist_aphabet(engine)];
   }
 
-  return random_string;
+  return random_ss.str();
 }
 
 size_t score(std::string_view str) {
-  std::accumulate(str.begin(), str.end(), 0, [](auto c) {if (c)});
   size_t count = 0;
   for (size_t i = 0; i < ans.size(); i++)
   {
@@ -47,11 +41,32 @@ size_t score(std::string_view str) {
   return count;
 }
 
-bool make_5percent() {
-  std::random_device seed_gen;
-  std::default_random_engine engine(seed_gen());
 
-  std::uniform_int_distribution<> dist(1, 100);
+void run() {
+  auto str = make_random_string();
+  auto s = score(str);
 
-  return dist(engine) <= 5;
+  std::vector<std::pair<std::string, size_t>> childs(30, std::make_pair(str, s));
+
+  int count = 0;
+  do
+  {
+    for (auto child = std::begin(childs); child != std::end(childs); child++)
+    {
+      make_mutatable_string((*child).first);
+      (*child).second = score((*child).first);
+    }
+
+    auto highest_score = std::max_element(childs.begin(), childs.end(), [](auto a, auto b) {
+      // std::cout << (a).first << ": " << (a).second << std::endl;
+      return a.second < b.second;
+      });
+
+    std::cout << count << "highest: " << (*highest_score).first << ": " << (*highest_score).second << std::endl;
+
+    std::vector<std::pair<std::string, size_t>> new_childs(30, *highest_score);
+    childs.swap(new_childs);
+
+    count++;
+  } while (!is_match(childs[0].first));
 }
