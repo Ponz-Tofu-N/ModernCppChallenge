467c1246f501f9f1d44f990e76df5a44f814394c
ponz-tofu-n <shi.na.telebla3@gmail.com>
2022-08-17 01:26:03 +0900

    add comparing scenario b/w non-thread & threaded

--- 061/main.cpp
+++ 061/main.cpp
@@ -1,8 +1,8 @@
-/* 60. Conway's Game of Life */
+/* 61. Threaded transform algorithm */
 
 /*
  * features
- * https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+ *  transform the elements of a range in paralell by a given function.
  */
 
 #include <vector>
@@ -11,20 +11,35 @@
 
 int main(int argc, char const* argv[])
 {
-  std::vector<int> items = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  std::vector<int> items(100000000, 2);
 
-  print(items);
+  auto start1 = std::chrono::system_clock::now();
+  std::transform(std::begin(items),
+                 std::end(items),
+                 items.begin(),
+                 [](auto& a) { return a * 2; });
 
-  thread_transform(std::begin(items),
-                   std::end(items),
-                   [](auto& a)
-                   {
-                     using namespace std::chrono_literals;
-                     std::this_thread::sleep_for(1000ms);
-                     return a * 2;
-                   });
+  auto end1 = std::chrono::system_clock::now();
+  auto erapsed_time1 =
+      std::chrono::duration_cast<std::chrono::milliseconds>(end1 - start1);
+  std::cout << "std::transform: " << erapsed_time1.count() << " ms"
+            << std::endl;
 
-  print(items);
+  auto start = std::chrono::system_clock::now();
+  thread_transform_ver2(std::begin(items),
+                        std::end(items),
+                        [](auto& a)
+                        {
+                          //  using namespace std::chrono_literals;
+                          //  std::this_thread::sleep_for(1000ms);
+                          return a * 2;
+                        });
+  auto end = std::chrono::system_clock::now();
+
+  auto erapsed_time =
+      std::chrono::duration_cast<std::chrono::seconds>(end - start);
+  std::cout << "threaded transform: " << erapsed_time.count() << " ms"
+            << std::endl;
 
   return 0;
 }
