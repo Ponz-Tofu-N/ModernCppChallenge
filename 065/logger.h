48487e596f0c334f1a335daafa3ab5933025539d
Ponz-Tofu-N <shi.na.telebla@gmail.com>
2022-10-04 15:13:10 +0900

    fix that mutex header is missing

--- 065/logger.h
+++ 065/logger.h
@@ -1,15 +1,16 @@
 #include <iostream>
+#include <mutex>
 #include <thread>
 
 class Logger
 {
- public:
+public:
   static Logger& instance()
   {
     static Logger log;
     return log;
   }
-  Logger(const Logger& other)         = delete;
+  Logger(const Logger& other) = delete;
   void operator=(const Logger& other) = delete;
 
   template <typename... T>
@@ -24,7 +25,19 @@ class Logger
     lock.unlock();
   }
 
- private:
+private:
   constexpr Logger() {}
   std::mutex lock;
 };
+
+// mutexロックがないときは出力の間に他の出力が発生する場合がある。
+// [log] 1: 0 2: 1
+// [log] 1: 1 2: 0
+// [log] 1: 2 2: 9
+// [log] 1: 4 2: 8
+// [log] 1: 5 2: 2
+// [log] 1: 6 2: 7
+// [log] [log] 1: 8 2: 5
+// [log] 1: 9 2: 3
+// [log] 1: 7 2: 10
+// 1: 3 2: 1
